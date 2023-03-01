22487b364a484a11268870fb1ed23831a6ec5b1d
Your Name <justin.georgilas@gmail.com>
2023-02-28 20:57:15 -0800

    adding character selection

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-const inventory = newInventory()
-move(inventory).to(0, 0)
+let firstAnswer  = window.prompt('Please select a character? Gnome, Tree or Pillar')
+if(firstAnswer === 'Tree'){
+ //Need to add code to switch the green character to a tree   
+} 
+else if(firstAnswer === 'gnome') {
+  // I can keep the default settings
+}else (firstAnswer === 'Pillar') {
+// change to pillar
+}
 
-const character = newImage('assets/green-character/static.gif')
-let direction = null;
-let x = 100;
-let y = 250;
-function moveCharacter () {
-if(direction === 'west') {
-    x = x-1;
-}
-if(direction === 'north') {
-    y = y+1;
-}
-if(direction === 'east') {
-    x = x+1;
-}
-if(direction === 'south') {
-    y = y-1;
-}
-character.style.left = x + 'px';
-character.style.bottom = x + 'px';
-}
-//move(character).to(100, 250)
+const inventory = newInventory();
+move(inventory).to(0, 0);
 
+const character = newImage("green-character/static.gif");
 
-move(newImage('assets/tree.png')).to(200, 450)
-move(newImage('assets/pillar.png')).to(350, 250)
-move(newImage('assets/pine-tree.png')).to(450, 350)
-move(newImage('assets/crate.png')).to(150, 350)
-move(newImage('assets/well.png')).to(500, 575)
-move(newItem('assets/sword.png')).to(500, 555)
-move(newItem('assets/shield.png')).to(165, 335)
-move(newItem('assets/staff.png')).to(600, 250)
+function handleDirectionChange(direction) {
+  if (direction === null) {
+    character.src = "assets/green-character/static.gif";
+  }
+  if (direction === "west") {
+    character.src = "assets/green-character/west.gif";
+  }
+  if (direction === "north") {
+    character.src = "assets/green-character/north.gif";
+  }
+  if (direction === "east") {
+    character.src = "assets/green-character/east.gif";
+  }
+  if (direction === "south") {
+    character.src = "assets/green-character/south.gif";
+  }
+}
+
+move(character).withArrowKeys(100, 250, handleDirectionChange);
+
+move(newImage("assets/tree.png")).to(200, 450);
+move(newImage("assets/pillar.png")).to(350, 250);
+move(newImage("assets/pine-tree.png")).to(450, 350);
+move(newImage("assets/crate.png")).to(150, 350);
+move(newImage("assets/well.png")).to(500, 575);
+move(newItem("assets/sword.png")).to(500, 555);
+move(newItem("assets/shield.png")).to(165, 335);
+move(newItem("assets/staff.png")).to(600, 250);
